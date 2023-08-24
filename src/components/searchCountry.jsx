e0a78d30e1954cd126dfdc86dea8297a2f16ce85
Timi <tunmisebabade@gmail.com>
2023-08-24 16:43:23 +0100

    first commit

--- src/components/searchCountry.jsx
+++ src/components/searchCountry.jsx
@@ -1,37 +1,61 @@
 import { FcAutomotive } from "react-icons/fc";
+import { motion } from "framer-motion";
 // import { FaRegUserCircle,FaShoppingCart} from 'react-icons/fa';
 const COMMON_CLASS = "flex items-center gap-3 my-2"
 
 
+const countryVariant = {
+    first: {
+        y: '60px',
+    },
+    last: {
+        y: 0,
+        transition: {
+            type: 'spring', stiffness: 200, staggerChildren: 0.2, when: "beforeChildren"
+        }
+    }
+}
+
+const childrenVariant = {
+    first: {
+        x: '100px',
+        opacity: 0
+    },
+    last: {
+        x: 0,
+        opacity: 1
+    }
+}
+
 export default function SearchCountryData({name, area, car, population, region, subregion, startOfWeek, flags}) {
     return (
-        <div className="p-2 h-fit lg:p-3 shadow-md border-b-black border-b-2 border-2">
-            <div className="flex item-center justify-between gap-3">
+        <motion.div variants={countryVariant} initial="first" animate="last" className="p-2 h-fit lg:p-3 shadow-md border-b-black border-b-2 border-2">
+            <motion.div variants={childrenVariant} className="flex item-center justify-between gap-3">
                 <p className="font-semibold text-lg h-12 my-2">{name.official.toUpperCase()}</p>
                 <p className="font-semibold text-md text-right my-2">{area}km<sup>2</sup></p>
-            </div>
-            <img src={flags.png} alt="" className="w-full h-40 object-cover "/>
-            <div className={`${COMMON_CLASS}`}>
+            </motion.div>
+            <motion.img variants={childrenVariant} src={flags.png} alt="" className="w-full h-40 object-cover "/>
+            <motion.div variants={childrenVariant} className={`${COMMON_CLASS}`}>
                 <h1 className="text-md font-bold">NAME:</h1>
                 <p className="font-semibold">{name.common}</p>
-            </div>
-            <div className={`${COMMON_CLASS}`}>
+            </motion.div>
+            <motion.div variants={childrenVariant} className={`${COMMON_CLASS}`}>
                 <h1 className="text-md font-bold">POPULATION:</h1>
                 <p className="font-semibold">{population}</p>
-            </div>
-            <div className={`${COMMON_CLASS}`}>
+            </motion.div>
+            <motion.div variants={childrenVariant} className={`${COMMON_CLASS}`}>
                 <h1 className="text-md font-bold">REGION:</h1>
                 <p className="font-semibold">{region}</p>
-            </div>
-            <div className={`${COMMON_CLASS}`}>
+            </motion.div>
+            <motion.div variants={childrenVariant} className={`${COMMON_CLASS}`}>
                 <h1 className="text-md font-bold">SUB-REGION:</h1>
                 <p className="font-semibold">{subregion}</p>
-            </div>
-            <div className={`${COMMON_CLASS}`}>
+            </motion.div>
+            <motion.div variants={childrenVariant} className={`${COMMON_CLASS}`}>
                 <h1 className="text-md font-bold">START OF THE WEEK:</h1>
                 <p className="font-semibold">{startOfWeek}</p>
-            </div> 
-            <p className="flex items-center gap-2 font-bold"> <FcAutomotive size={30}/>{car.side}</p>
-        </div>
+            </motion.div> 
+            <motion.p variants={childrenVariant} className="flex items-center gap-2 font-bold"> <FcAutomotive size={30}/>{car.side}</motion.p>
+        </motion.div>
     )
 }
