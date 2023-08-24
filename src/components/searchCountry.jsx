f5255e768076fc9fe92da4b0f4493ee5c9fe9d73
Timi <tunmisebabade@gmail.com>
2023-08-24 17:21:03 +0100

    first commit

--- src/components/searchCountry.jsx
+++ src/components/searchCountry.jsx
@@ -27,17 +27,24 @@ const childrenVariant = {
     }
 }
 
-export default function SearchCountryData({name, area, car, population, region, subregion, startOfWeek, flags}) {
+export default function SearchCountryData({name, area, car, capital, coatOfArms, population, region, subregion, startOfWeek, flags}) {
     return (
-        <motion.div variants={countryVariant} initial="first" animate="last" className="p-2 h-fit lg:p-3 shadow-md border-b-black border-b-2 border-2">
+        <motion.div variants={countryVariant} initial="first" animate="last" className="p-2 h-fit md:h-auto  lg:p-3 shadow-md border-b-black border-b-2 border-2">
             <motion.div variants={childrenVariant} className="flex item-center justify-between gap-3">
                 <p className="font-semibold text-lg h-12 my-2">{name.official.toUpperCase()}</p>
                 <p className="font-semibold text-md text-right my-2">{area}km<sup>2</sup></p>
             </motion.div>
-            <motion.img variants={childrenVariant} src={flags.png} alt="" className="w-full h-40 object-cover "/>
+            <motion.img variants={childrenVariant} src={flags.png} alt="" className="w-full h-40 object-cover"/>
+            <motion.div variants={childrenVariant} className={`${COMMON_CLASS} justify-between`}>
+                <motion.div variants={childrenVariant} className="flex items-center gap-3">
+                    <h1 className="text-md font-bold">NAME:</h1>
+                    <p className="font-semibold">{name.common}</p>
+                </motion.div>
+                <motion.img initial={{opacity:0}} animate={{opacity:1}} variants={childrenVariant} className="w-10" src={coatOfArms.png} alt="" />
+            </motion.div>
             <motion.div variants={childrenVariant} className={`${COMMON_CLASS}`}>
-                <h1 className="text-md font-bold">NAME:</h1>
-                <p className="font-semibold">{name.common}</p>
+                <h1 className="text-md font-bold">CAPITAL:</h1>
+                <p className="font-semibold">{capital}</p>
             </motion.div>
             <motion.div variants={childrenVariant} className={`${COMMON_CLASS}`}>
                 <h1 className="text-md font-bold">POPULATION:</h1>
