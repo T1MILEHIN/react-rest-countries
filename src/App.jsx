aaa7ffccba91b63a15f926a3e882ffc151c8b752
Timi <tunmisebabade@gmail.com>
2023-08-26 10:58:15 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: "",
     element: < Nav/>,
     errorElement: <ErrorElement />,
     children: [
